2876ca6e62535f039d0ef45acba8d8e889496218
chgagnon <chgagnon@microsoft.com>
2020-12-08 17:47:44 -0800

    await activation

--- extensions/resource-deployment/src/test/services/apiService.test.ts
+++ extensions/resource-deployment/src/test/services/apiService.test.ts
@@ -3,12 +3,15 @@
  *  Licensed under the Source EULA. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import * as vscode from 'vscode';
+import * as azurecore from 'azurecore';
 import 'mocha';
 import { apiService } from '../../services/apiService';
 import * as should from 'should';
 
 describe('API Service Tests', function (): void {
 	it('get azurecoreApi returns azure api', async function () {
+		await vscode.extensions.getExtension(azurecore.extension.name)?.activate();
 		const api = apiService.azurecoreApi;
 		should(api).not.be.undefined();
 	});
