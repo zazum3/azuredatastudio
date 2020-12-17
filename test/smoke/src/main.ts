35d1e63bf552962a95b477284c6368b61a716612
Lucy Zhang <luczhan@microsoft.com>
2020-12-17 09:01:03 -0800

    logs

--- test/smoke/src/main.ts
+++ test/smoke/src/main.ts
@@ -240,6 +240,7 @@ function createOptions(): ApplicationOptions {
 		loggers.push(new FileLogger(opts.log));
 		log = 'trace';
 	}
+	loggers.push(new FileLogger('C:\\Users\\luczhan\\GitProjects\\azuredatastudio\\smoke_log.txt'));
 	return {
 		quality,
 		codePath: opts.build,
